97f5bfd90887f100f69c300b474cecda0e5b066a
laphatradap <laphatradap@disroot.org>
2020-05-20 13:26:39 +0200

    Deploy to Heroku

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const databaseUrl = process.env.DATABASE_URL || "postgres://postgres:secret@loca
 
 const db = new Sequelize(databaseUrl);
 
-db.sync({force: true})
+db.sync({force: false})
   .then(() => console.log("DB schema updated"))
   .catch(console.error);
 
